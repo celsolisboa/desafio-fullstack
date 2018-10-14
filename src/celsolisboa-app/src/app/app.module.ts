a983faa437b84dc4a3b042b6002a96d25339286d
gabrielcabralbfr <gabrielcabralbfr@gmail.com>
2018-10-14 00:39:49 -0300

    [add] login component

--- src/celsolisboa-app/src/app/app.module.ts
+++ src/celsolisboa-app/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login-component/login-component.component';
+import { LoginComponent } from './login-component/login.component';
 
 import { ROUTES } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoComponent } from './cursos/curso/curso.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CursosComponent,
+    CursoComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     RouterModule.forRoot(ROUTES),
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
