eb1d6bfaf0f6c5573830715314ccde596c2c4ab0
gabrielcabralbfr <gabrielcabralbfr@gmail.com>
2018-10-14 00:39:31 -0300

    [add] configuração de rotas

--- src/celsolisboa-app/src/app/app.routes.ts
+++ src/celsolisboa-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router'
-import { LoginComponent } from './login-component/login-component.component';
+import { LoginComponent } from './login-component/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 export const ROUTES: Routes = [
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'cursos', component: CursosComponent }
 ]
