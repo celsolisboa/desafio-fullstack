3b9b100e650db32e61b28579c873013da23a70a8
gabrielcabralbfr <gabrielcabralbfr@gmail.com>
2018-10-15 19:48:43 -0300

    [improve] change server to pool connection

--- src/celsolisboa-app/server.js
+++ src/celsolisboa-app/server.js
@@ -1,85 +1,77 @@
-const database = require("mssql/msnodesqlv8");
-const config = {
+const database = require("mssql/msnodesqlv8")
+
+const port = 3000
+
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const path = require("path")
+
+const dbConfig = {
     driver: 'msnodesqlv8',
     user: 'gabriel',
     password: '$smvg0000',
     server: 'db-server-mssql.database.windows.net',
     database: 'db-celsolisboa-app',
-};
-const port = 3000;
+}
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors")
-const path = require("path")
-
-const app = express();
+const app = express()
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-const router = express.Router();
+const router = express.Router()
 
 router.get("/", (req, res) => {
-    // database.connect(config)
-    //     .then(() => res.send("Sucesso"))
-    //     .catch((error) => console.log(error))
-
     res.send("Sucesso")
 });
 
 router.get("/teste", (req, res) => {
-    database.connect(config)
-        .then(conn => {
-            const request = new database.Request()
-            request.query('select * from teste', (error, recordSet) => {
-                res.json(recordSet.recordsets[0])
-                database.close();
-            });
+    new database.ConnectionPool(dbConfig).connect()
+        .then(pool => {
+            return pool.query('select * from teste')
         })
-        .catch(() => console.log("erro"));
-
-});
-
-router.delete("/api/user/delete/:id", (req, res) => {
-    database.connect(config)
-        .then(conn => {
-            const request = new database.Request();
-            request.query('delete teste2 where ID=' + parseInt(req.params.id), (error, recordSet) => {
-                res.send("usuário deletado com sucesso")
-                database.close()
-            });
+        .then(result => {
+            console.log('Resultado: ' + result)
+            res.json(result.recordsets[0])
+            res.end()
         })
-        .catch(() => console.log("erro"));
-
-});
+        .catch(error => console.log(error))
+})
 
 router.post("/api/cursos/cadastrar", (req, res) => {
-    console.log(req.body.startDate);
-    database.connect(config)
-        .then(conn => {
-            const request = new database.Request();
-            let q = `insert into teste(id, nome, professor, sala, data_inicio, data_fim) 
-            VALUES (${req.body.id}, '${req.body.courseName}', '${req.body.teachers}', '${req.body.rooms}', '${req.body.startDate}', '${req.body.endDate}')`;
-            console.log(q)
-            request.query(q, (error, recordSet) => {
-                if (error) {
-                    console.log(error);
-                } else {
 
-                    res.send(`usuário criado com sucesso: ${req.body.nome}`)
-                }
-                database.close()
-            });
+    new database.ConnectionPool(dbConfig).connect()
+        .then(pool => {
+            const queryString = `insert into 
+            teste(id, nome, professor, sala, inicio, fim) 
+            VALUES (${req.body.id}, '${req.body.courseName}', '${req.body.teachers}', '${req.body.rooms}', '${req.body.startDate}', '${req.body.endDate}')`
+            console.log(queryString)
+            return pool.query(queryString)
         })
-        .catch((error) => console.log(error));
+        .then(result => {
+            console.log(result.recordsets[0])
+            res.json(result.recordsets[0])
+            res.end()
+        })
+        .catch(error => console.log(error))
+})
+
+router.delete("/api/cursos/deletar/:id", (req, res) => {
+    new database.ConnectionPool(dbConfig).connect()
+        .then(pool => {
+            console.log("fazendo a query de delete")
+            return pool.query `delete teste where ID=${parseInt(req.params.id)}`
+        })
+        .then(() => console.log("Deletado com sucesso!") && res.end())
+        .catch(error => console.log(error))
 
 });
 
-app.use('/', router);
-app.use('/teste', router);
-app.use('/user/create', router);
-app.use('/user/delete/:id', router);
+app.use('/', router)
+app.use('/teste', router)
+app.use('/api/cursos/cadastrar', router)
+app.use('/api/cursos/deletar/:id', router)
 
-app.listen(port, () => console.log("Servidor ouvindo"));
+app.listen(port, () => console.log("Servidor ouvindo"))
