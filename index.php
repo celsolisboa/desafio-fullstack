4a3ff9c3a3a2c39208c7ed0584591a380b01e174
Marcello Lyrio <marcellolyrio@hotmail.com>
2018-12-08 19:17:38 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/login.php">
+<title>API Cursos</title>
+<script language="javascript">
+    window.location.href = "pages/login.php"
+</script>
+</head>
+<body>
+Go to <a href="pages/login.php">Login</a>
+</body>
+</html>
