6c4035a453280d7f9786988f92a4d674936f4562
Marcello Lyrio <marcellolyrio@hotmail.com>
2018-12-08 20:48:23 -0200

    Update config.php
    Corrigindo os parametros de acesso ao banco de dados.

--- api/config/config.php
+++ api/config/config.php
@@ -2,8 +2,7 @@
 	
 	define('DB_NAME','apicursos');
 	define('DB_HOST', 'localhost');
-	define('DB_PASS','l015077');
-	define('DB_USER','root');
-
+	define('DB_USER','');
+	define('DB_PASS','');
 ?>
 
