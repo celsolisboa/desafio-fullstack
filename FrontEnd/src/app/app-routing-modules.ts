2878e798dc9f892223d9f31550f3c3c667643810
helio <helio_dantas@outlook.com>
2019-01-27 13:39:10 -0200

    tela de login

--- FrontEnd/src/app/app-routing-modules.ts
+++ FrontEnd/src/app/app-routing-modules.ts
@@ -1,9 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CursosComponent } from './curso/cursos/cursos.component';
+import { LoginComponent } from './auth/login/login.component';
+
 
 const routes: Routes = [
-      {path: '', component: CursosComponent }
+      {path: '', component: LoginComponent }
 
 ];
 
--- FrontEnd/src/app/auth/auth.module.ts
+++ FrontEnd/src/app/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ]
 })
 export class AuthModule { }
