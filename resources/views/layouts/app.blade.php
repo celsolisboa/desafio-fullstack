3c14d74bdbc346853393b1c8598535441209fe8c
Christian Possidonio <christian@orientme.com.br>
2019-02-07 19:19:10 -0200

    Adicionando a pasta Themecom o estilo do sistema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        
                     </ul>
 
                     <!-- Right Side Of Navbar -->
