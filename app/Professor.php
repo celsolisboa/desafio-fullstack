bad914d857f24b8486d1e5a67e8c3deeb951b0bd
Christian Possidonio <christian@orientme.com.br>
2019-02-07 16:32:16 -0200

    Na minha model Professor eu tive que indicar a tabela que eu quero usar, adicionando uma variável protegida, pois o migrations que executei usando o php artisan adiciona por padrão o S no final da palavra e usa para referencia a tabela no banco, porém eu mudei de professors para PROFESSORES e por este motivo tive que indicar isto no model Professor

--- app/Professor.php
+++ app/Professor.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professor extends Model
 {
-    //
+    /* Por padrão o laravel adicionaria o S para encontrar a table gerada no migrations, mas o padrão 
+    *  que ele criar é adicionando apenas o S no final da palavra ficando PROFESSORS mas tive que mudar para PROFESSORES
+    *  e por este motivo eu estou abaixo criando uma variável protegida com o nome da tabela no banco. 
+    */
+    protected $table = "professores";
 }
