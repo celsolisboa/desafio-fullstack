77a2cd9df47126baf2b0dfd87175ea1510017b32
Christian Possidonio <christian@orientme.com.br>
2019-02-07 15:52:15 -0200

    Eu tive que alterar o nome do migration pois por padrão o laravel criar todos os migrations no plural quando usamos o comando php artisan para geral os migrations para a sua regra, porém no inglês temos apenas que adicionar a letra S no final de cada palavra para transforma-la em plural mas no portugues sabemos que não é bem assim, então eu fiz a ALTERAÇÃO de professors para professores

--- database/migrations/2019_02_07_171549_create_professores_table.php
+++ database/migrations/2019_02_07_171549_create_professores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfessorsTable extends Migration
+class CreateProfessoresTable extends Migration
 {
     /**
      * Run the migrations.
