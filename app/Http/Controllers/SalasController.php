6937fa1e5369cfbda38ea468c3fbc06ea1bdf112
Christian Possidonio <christian@orientme.com.br>
2019-02-07 20:28:37 -0200

    Criado o metodo index, que apenas listas as salas e return um array dentro da variável

--- app/Http/Controllers/SalasController.php
+++ app/Http/Controllers/SalasController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Sala;
 class SalasController extends Controller
 {
-    //
+    public function index(){
+        $sala = Sala::all();
+        if( isset( $sala ) ):
+            return view('salas', compact('sala'));
+        else:
+            $erro = "Não encontramos nenhum registro na base de dados";    
+        endif;        
+    }
 }
