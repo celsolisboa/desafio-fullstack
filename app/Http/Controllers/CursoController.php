d9fbbc69f813b5e98e070e2b71b6f7752faf3d84
Christian Possidonio <christian@orientme.com.br>
2019-02-07 21:22:30 -0200

    Middleware de verificação de login ativado

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
-{
+{   
+    public function __construct(){
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
