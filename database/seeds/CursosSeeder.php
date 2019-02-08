c39ce2de5b67e3cac8cce2eab81f48cf8e0491df
Christian Possidonio <christian@orientme.com.br>
2019-02-08 12:44:12 -0200

    Ao alterar algumas colunas da tabela cursos eu tive que otimizar para a geração de seeders

--- database/seeds/CursosSeeder.php
+++ database/seeds/CursosSeeder.php
@@ -11,7 +11,8 @@ class CursosSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('cursos')->insert(['sala_id' => 1, 'prof_id' => 1, 'nome' => 'Biologia', 'inicio' => '18:00', 'fim' => '18:45']);
-        DB::table('cursos')->insert(['sala_id' => 2, 'prof_id' => 2, 'nome' => 'Português', 'inicio' => '19:00', 'fim' => '19:45']);
+        DB::table('cursos')->insert(['sala_id' => 1, 'professor_id' => 1, 'nome_curso' => 'Biologia', 'inicio' => '18:00', 'fim' => '18:45']);
+        DB::table('cursos')->insert(['sala_id' => 2, 'professor_id' => 2, 'nome_curso' => 'Português', 'inicio' => '19:00', 'fim' => '19:45']);
+        DB::table('cursos')->insert(['sala_id' => 3, 'professor_id' => 3, 'nome_curso' => 'Física', 'inicio' => '19:50', 'fim' => '20:25']);
     }
 }
