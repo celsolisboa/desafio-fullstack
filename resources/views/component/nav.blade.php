d3a8d06e2feafcad8f637805035ff72fbcefd4ba
Christian Possidonio <christian@orientme.com.br>
2019-02-08 12:09:11 -0200

    Alterações no menu

--- resources/views/component/nav.blade.php
+++ resources/views/component/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Sistema de Cursos
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
