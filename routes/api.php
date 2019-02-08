d21a41d1cd9770aac0a9de7139e6c1a43696be9c
Christian Possidonio <christian@orientme.com.br>
2019-02-08 03:40:14 -0200

    Criei uma api listando os cursos e retornando em Json para acesso externo

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,18 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Use App\Curso;
+Use App\Sala;
+Use App\Professor;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Está api retorna todos as informações do professores, que estão ligados as salas e cursos.
+Route::get('/cursos', function(){
+    $cur = Professor::with('salas')->get();    
+    return $cur->toJson();
+});
+
+
