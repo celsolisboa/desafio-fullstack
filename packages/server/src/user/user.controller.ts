f28789b11161b25f259599b8beddfb5c65662fd0
Gabriel Souza <gabrielsfarias@outlook.com>
2022-04-10 17:09:02 -0300

    refactor: implements basic validation at request body fields

--- packages/server/src/user/user.controller.ts
+++ packages/server/src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Get, Param } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get(':id')
-  async findOne(@Body('email') email: string) {
-    const user = await this.userService.findOne(email);
+  @Get()
+  async findOne(@Body() email: UserDto) {
+    const user = await this.userService.findByLogin(email);
     delete user['password'];
     return user;
   }
