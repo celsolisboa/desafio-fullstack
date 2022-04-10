8953b27435eddde11421866c6820573ce70084b1
bartcosta <costakrats@gmail.com>
2022-04-10 19:00:14 -0300

    refactor: database config

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -5,54 +5,54 @@
  * file.
  */
 
- import Env from '@ioc:Adonis/Core/Env'
- import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
- 
- const databaseConfig: DatabaseConfig = {
-   /*
-   |--------------------------------------------------------------------------
-   | Connection
-   |--------------------------------------------------------------------------
-   |
-   | The primary connection for making database queries across the application
-   | You can use any key from the `connections` object defined in this same
-   | file.
-   |
-   */
-   connection: Env.get('DB_CONNECTION'),
- 
-   connections: {
-     /*
-     |--------------------------------------------------------------------------
-     | PostgreSQL config
-     |--------------------------------------------------------------------------
-     |
-     | Configuration for PostgreSQL database. Make sure to install the driver
-     | from npm when using this connection
-     |
-     | npm i pg
-     |
-     */
-     pg: {
-       client: 'pg',
-       connection: {
-         host: Env.get('DB_HOST'),
-         port: Env.get('DB_PORT'),
-         user: Env.get('DB_USER'),
-         password: Env.get('DB_PASSWORD', ''),
-         database: Env.get('DB_DATABASE'),
-       },
-       migrations: {
-         naturalSort: true,
-         paths: [
-           './database/migrations',
-         ]
-       },
-       healthCheck: false,
-       debug: false,
-     },
- 
-   }
- }
- 
- export default databaseConfig
+import Env from '@ioc:Adonis/Core/Env'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+
+const databaseConfig: DatabaseConfig = {
+  /*
+  |--------------------------------------------------------------------------
+  | Connection
+  |--------------------------------------------------------------------------
+  |
+  | The primary connection for making database queries across the application
+  | You can use any key from the `connections` object defined in this same
+  | file.
+  |
+  */
+  connection: Env.get('DB_CONNECTION'),
+
+  connections: {
+    /*
+    |--------------------------------------------------------------------------
+    | PostgreSQL config
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for PostgreSQL database. Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i pg
+    |
+    */
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('DB_HOST'),
+        port: Env.get('DB_PORT'),
+        user: Env.get('DB_USER'),
+        password: Env.get('DB_PASSWORD', ''),
+        database: Env.get('DB_DATABASE'),
+      },
+      migrations: {
+        naturalSort: true,
+        paths: [
+          './database/migrations',
+        ]
+      },
+      healthCheck: false,
+      debug: false,
+    },
+
+  }
+}
+
+export default databaseConfig
