d454fa5a648b4da455e3887cb687733db9bb6e82
bartcosta <costakrats@gmail.com>
2022-04-11 05:45:56 -0300

    refactor: Course Service

--- backend/app/Services/CourseService.ts
+++ backend/app/Services/CourseService.ts
@@ -1,16 +1,24 @@
 import Courses from "App/Models/Course";
 
+type CoursesType = {
+    name: string, 
+    class_room: string, 
+    beginning: string, 
+    end: string, 
+    teacher_id: string
+}
+
 export default class CourseService {
-    public static async create (name: string, class_room: string, beginning: string, end: string, teacher_id: string){
-       const course = new Courses()
+    public static async create (course: CoursesType){
+       const newCourse = new Courses()
 
-       course.name = name
-       course.class_room = class_room
-       course.beginning = beginning
-       course.end = end
-       course.teacher_id = teacher_id
+       newCourse.name = course.name
+       newCourse.class_room = course.class_room
+       newCourse.beginning = course.beginning
+       newCourse.end = course.end
+       newCourse.teacher_id = course.teacher_id
 
-       await course.save()
+       await newCourse.save()
     }
 
 
