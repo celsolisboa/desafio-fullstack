495d080bffa21afbdab14b196dd2c43b0efc66f3
bartcosta <costakrats@gmail.com>
2022-04-11 00:53:21 -0300

    feat: Pages Logon and delete api.js

---
+++ frontend/src/Pages/Logon/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './styles.css';
+
+export default function Logon(){
+
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const navigate = useNavigate();
+
+    async function handleLogin(){
+        navigate('/courses');
+    }
+    
+    return(
+        <body>
+
+            <div id="page">
+
+                <div className = "logon-container">
+
+                    <h1>Login</h1>
+
+                    <form onSubmit={handleLogin}>
+                        <div className="input-block">
+                            <input
+                                placeholder = "E-mail"
+                                type="email"
+                                value={email}
+                                onChange={ e => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+ 
+                        <div className="input-block">
+                            <input
+                                placeholder = "Senha"
+                                type="password"
+                                minLength={6}
+                                value={password}
+                                onChange={ e => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <button className="button" type="submit">Entrar</button>
+                        
+                    </form>
+
+                </div>
+
+            </div>
+            
+        </body>   
+        
+
+    );
+}
