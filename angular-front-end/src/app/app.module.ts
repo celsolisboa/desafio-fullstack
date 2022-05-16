8971c31300a41a7f86ff342c3aff156aebe62b0d
Gustavo Henrique <gust.leles2@gmail.com>
2022-05-15 23:43:53 -0300

    Criação do footer e do header

--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
