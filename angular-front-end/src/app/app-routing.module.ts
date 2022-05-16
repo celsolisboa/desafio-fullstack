6e52ceb857ccc9c4a22f6f572cc64ac5ce06a54c
Gustavo Henrique <gust.leles2@gmail.com>
2022-05-15 23:48:59 -0300

    Configurando as rotas

--- angular-front-end/src/app/app-routing.module.ts
+++ angular-front-end/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursoComponent } from './components/pages/curso/curso.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { NovoCursoComponent } from './components/pages/novo-curso/novo-curso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'cursos', component: CursoComponent},
+  {path: 'curso/novo-curso', component: NovoCursoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
