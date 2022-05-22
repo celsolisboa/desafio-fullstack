4f7514231b369fe057700b954d95fd36aca414aa
davimaia42 <davi.mendonca.maia@gmail.com>
2022-05-22 19:19:01 -0300

    feat(teacher): add findByIds method on teachers repository

--- back-end/src/repositories/ITeachersRepository.ts
+++ back-end/src/repositories/ITeachersRepository.ts
@@ -2,4 +2,5 @@ import { Teacher } from "../entities/Teacher";
 
 export interface ITeachersRepository{
     list(): Promise<Teacher[]>;
+    findByIds(ids: string[]): Promise<Teacher[]>;
 }
--- back-end/src/repositories/implementations/PostgresTeachersRepository.ts
+++ back-end/src/repositories/implementations/PostgresTeachersRepository.ts
@@ -1,16 +1,20 @@
-import { Repository } from "typeorm";
+import { Repository, Any } from "typeorm";
 import { AppDataSource } from "../../database";
 import { Teacher } from "../../entities/Teacher";
 import { ITeachersRepository } from "../ITeachersRepository";
 
 export class PostgresTeachersRepository implements ITeachersRepository {
-    TeachersRepository: Repository<Teacher>;
+    teachersRepository: Repository<Teacher>;
 
     constructor() {
-        this.TeachersRepository = AppDataSource.getRepository(Teacher);
+        this.teachersRepository = AppDataSource.getRepository(Teacher);
     }
 
     async list(): Promise<Teacher[]> {
-        return await this.TeachersRepository.find();
+        return await this.teachersRepository.find();
+    }
+
+    async findByIds(ids: string[]): Promise<Teacher[]> {
+        return await this.teachersRepository.findBy({ id: Any(ids) });
     }
 }
