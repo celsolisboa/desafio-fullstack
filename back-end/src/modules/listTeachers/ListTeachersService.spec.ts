dbbe25853bf0fe51e4d33bef29784604193b58f7
davimaia42 <davi.mendonca.maia@gmail.com>
2022-05-22 15:11:45 -0300

    feat(teacher): add unit test for ListTeachersService

---
+++ back-end/src/modules/listTeachers/ListTeachersService.spec.ts
@@ -0,0 +1,35 @@
+import { ListTeachersService } from "./ListTeachersService";
+import { ITeachersRepository } from "../../repositories/ITeachersRepository";
+import { Teacher } from "../../entities/Teacher";
+
+class TeachersRepositoryStub implements ITeachersRepository {
+    teachersList: Array<Teacher>;
+
+    constructor(expectedData: Array<Teacher>) {
+        this.teachersList = expectedData;
+    }
+
+    async list(): Promise<Teacher[]> {
+        return this.teachersList;
+    }
+}
+
+describe("List Teachers", () => {
+    let sut: ListTeachersService;
+    let expectedData: Array<Teacher>;
+
+    beforeAll(() => {
+        expectedData = new Array<Teacher>();
+
+        for(let i = 1; i <= 3; i++) {
+            expectedData.push(new Teacher({ name: `test ${i}`}));
+        }
+        sut = new ListTeachersService(new TeachersRepositoryStub(expectedData));
+    });
+
+    it('should return a list of added teachers', async () => {
+        const teachers = await sut.execute();
+
+        expect(teachers).toEqual(expectedData);
+    })
+})
