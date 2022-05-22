e197a8360a55f869fafced12e91e9187969843d0
davimaia42 <davi.mendonca.maia@gmail.com>
2022-05-22 20:54:24 -0300

    fix(course): add empty array guard clause on CreateCourseService

--- back-end/src/modules/createCourse/CreateCourseService.ts
+++ back-end/src/modules/createCourse/CreateCourseService.ts
@@ -5,11 +5,11 @@ export class CreateCourseService {
     constructor(private coursesRepository: ICoursesRepository) {}
 
     async execute(data: ICreateCourseDTO) {
-        if (!data.teacher_ids) {
+        if (!data.teacher_ids || data.teacher_ids.length === 0) {
             throw new Error('A course must have at least one teacher');
         }
 
-        if (!data.classroom_ids) {
+        if (!data.classroom_ids || data.classroom_ids.length === 0) {
             throw new Error('A course must have at least one classroom');
         }
 
