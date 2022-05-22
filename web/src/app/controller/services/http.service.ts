52ebdba13e62b4b6d5a544a39fb62f4e57512400
Wesley <gifoniw@gmail.com>
2022-05-22 18:59:00 -0300

    Front end -Criação controller(Services/Models) -Configuração da requisição API para backend -Criado sistema de authenticação de usuario

---
+++ web/src/app/controller/services/http.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class HttpService {
+    apiURL: string = 'http://localhost:5432'
+
+    constructor (private http: HttpClient) { }
+
+    get(url: string) {
+        return this.http.get(`${this.apiURL}${url}`)
+    }
+    post(url: string, data: any) {
+        return this.http.post(`${this.apiURL}${url}`, data)
+    }
+    put(url: string, data: any) {
+        return this.http.put(`${this.apiURL}${url}`, data)
+    }
+    remove(url: string) {
+        return this.http.delete(`${this.apiURL}${url}`)
+    }
+}
