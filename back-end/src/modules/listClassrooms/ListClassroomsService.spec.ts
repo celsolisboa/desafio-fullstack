12bec0286585a7da9b2fa0f11983c9d1b1ab45a3
davimaia42 <davi.mendonca.maia@gmail.com>
2022-05-22 16:45:31 -0300

    feat(classroom): add unit test for ListClassroomsService

---
+++ back-end/src/modules/listClassrooms/ListClassroomsService.spec.ts
@@ -0,0 +1,35 @@
+import { ListClassroomsService } from "./ListClassroomsService";
+import { IClassroomsRepository } from "../../repositories/IClassroomsRepository";
+import { Classroom } from "../../entities/Classroom";
+
+class ClassroomsRepositoryStub implements IClassroomsRepository {
+    classroomsList: Array<Classroom>;
+
+    constructor(expectedData: Array<Classroom>) {
+        this.classroomsList = expectedData;
+    }
+
+    async list(): Promise<Classroom[]> {
+        return this.classroomsList;
+    }
+}
+
+describe("List Classrooms", () => {
+    let sut: ListClassroomsService;
+    let expectedData: Array<Classroom>;
+
+    beforeAll(() => {
+        expectedData = new Array<Classroom>();
+
+        for(let i = 1; i <= 3; i++) {
+            expectedData.push(new Classroom({ number: i.toString() }));
+        }
+        sut = new ListClassroomsService(new ClassroomsRepositoryStub(expectedData));
+    });
+
+    it('should return a list of added classrooms', async () => {
+        const classrooms = await sut.execute();
+
+        expect(classrooms).toEqual(expectedData);
+    })
+})
