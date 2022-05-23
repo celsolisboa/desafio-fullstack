55ee65df6e17fa172249ea9d2cccc0d6d44661f2
davimaia42 <davi.mendonca.maia@gmail.com>
2022-05-23 09:12:28 -0300

    feat(api): add cors on app

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import "reflect-metadata";
 import "./database";
@@ -7,6 +8,7 @@ import { routes } from "./routes";
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
