c534dbdcc988df98e897d370a23c85f17f653d32
Andresantos01 <andresanto.dev@gmail.com>
2022-05-25 18:50:43 -0300

    Criado as iInterfaces de Curso, Professor e Sala
    funcionalidade de cadastro de curso  criada na views/criar-curso/criar-curso.component.ts.
    multiselect dinâmico.

--- frontend/src/app/curso.service.ts
+++ frontend/src/app/curso.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Icurso } from './Icursos';
+import { IProfessor } from './IProfessor';
+import { ISala } from './ISala';
+import { ICursoCreate } from './Icurso';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,19 +11,29 @@ export class CursoService {
 
   constructor(private httpClient: HttpClient) { }
 
+
+  criarCurso(nome_curso: string, inicio: string, fim: string ,professor_id: number, sala_id: number ){
+    return this.httpClient.post<ICursoCreate>('api/cursos', [nome_curso,inicio,fim,sala_id,professor_id])
+  }
+
+
   listarCursos(){
     return this.httpClient.get<Icurso[]>('api/cursos');
   }
 
 
-
   deletarCurso(id: number){
-      return this.httpClient.delete(`api/cursos/${id}`)
+    return this.httpClient.delete(`api/cursos/${id}`)
   }
 
 
   listarProfessores(){
-      return this.httpClient.get('api/professores')
+    return this.httpClient.get<IProfessor[]>('api/professores')
   }
+
+  listarSalas(){
+    return this.httpClient.get<ISala[]>('api/salas')
+  }
+
 
 }
