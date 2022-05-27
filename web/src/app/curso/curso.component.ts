51ffb92be24369ecc2853c87261b226018283b28
Wesley <gifoniw@gmail.com>
2022-05-26 23:21:19 -0300

    Front End -Finalizado pagina do curso.     •Feito a configuração dos cards um do lado do outro.     •Configuração do component Button/Edit/Lixeira -Feito uma estilização da Pagina de curso.     •Feito a responsividade da pagina.     •De acordo com cada curso que venha ser inserido.

--- web/src/app/curso/curso.component.ts
+++ web/src/app/curso/curso.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/controller/services/api.service';
 
-
 @Component({
   selector: 'app-curso',
   templateUrl: './curso.component.html',
-  styleUrls: ['./curso.component.css']
+  styleUrls: ['./curso.component.css'],
 })
 export class CursoComponent implements OnInit {
-
-  constructor(private service:ApiService) { }
+  constructor(private service: ApiService) {}
 
   readData: any;
 
-
   ngOnInit(): void {
-    this.service.getAllDataCurso().subscribe((res)=>{
+    this.service.getAllDataCurso().subscribe((res) => {
       console.log(res);
       this.readData = res.data;
     });
   }
-
 }
