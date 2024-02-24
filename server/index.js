e7b40d23cc0ae84faa672cf8ce524d143c6032e1
alansgoncalves <vekio.soft@gmail.com>
2024-02-24 19:16:35 -0300

    create postgresql connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const pool = require('./db');
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
