4b222f0fe05228485332c803db4021933d202f37
alansgoncalves <vekio.soft@gmail.com>
2024-02-24 23:39:27 -0300

    create query to add new course

--- server/index.js
+++ server/index.js
@@ -18,6 +18,20 @@ app.get('/cursos', async (req, res) => {
     }
 });
 
+// create a course
+app.post('/detalhes', async (req, res) => {
+    try {
+        const { curso, professor, sala, horainicio, horatermino } = req.body;
+        const newCourse = await pool.query(
+            'INSERT INTO grade_academica (curso, professor, sala, horainicio, horatermino) VALUES($1, $2, $3, $4, $5) RETURNING *',
+            [curso, professor, sala, horainicio, horatermino]
+        );
+        res.json(newCourse.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
